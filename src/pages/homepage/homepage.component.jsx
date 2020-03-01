4ead56d3f77b52e79cdeff368f392f1bd0f9b4b4
tarwin3v <tarwin.ev@gmail.com>
2020-03-01 17:20:09 +0100

    01.03

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+import Directory from "../../components/directory/directory.component";
+
+function HomePage() {
+  return <Directory />;
+}
+
+export default HomePage;
